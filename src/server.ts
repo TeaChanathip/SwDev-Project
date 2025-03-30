a76222dad511d3729ce09b3b63ff647bde51cfaf
TeaChanathip <teachanthip@gmail.com>
2025-03-30 12:39:54 +0700

    Initilize express entry point

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config({ path: "../configs/config.env" })
+
+const app = express()
+app.use(cors())
+
+// Body parser
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+const server = app.listen(PORT, () =>
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`,
+    ),
+)
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err: Error) => {
+    console.error(`Error: ${err.message}`)
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
