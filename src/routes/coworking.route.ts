80d2e6e41d51d22c9208eb0314a7482e05e8034f
TeaChanathip <teachanthip@gmail.com>
2025-04-03 21:04:59 +0700

    Add get in coworking route

--- src/routes/coworking.route.ts
+++ src/routes/coworking.route.ts
@@ -2,6 +2,8 @@ import express from "express"
 import {
     createNewCoWorking,
     deleteCoWorking,
+    getAllCoWorkings,
+    getOneCoworking,
     updateCoWorking,
 } from "../controllers/coworking.controller"
 import { authorize } from "../middlewares/authorize.middleware"
@@ -13,5 +15,7 @@ const router = express.Router()
 router.post("/", protect, authorize(UserRole.ADMIN), createNewCoWorking)
 router.put("/:id", protect, authorize(UserRole.ADMIN), updateCoWorking)
 router.delete("/:id", protect, authorize(UserRole.ADMIN), deleteCoWorking)
+router.get("/", getAllCoWorkings)
+router.get("/:id", getOneCoworking)
 
 export default router
