0fda2c2cee07454d9f91c865cc12dfffab4ae2d7
TeaChanathip <teachanthip@gmail.com>
2025-04-18 00:52:46 +0700

    Refactor test cases in PM runner

---
+++ database-docker/mocks/rooms.sql
@@ -0,0 +1,66 @@
+-- CoWorking1
+INSERT INTO "room" (name, capacity, price, coworking_id)
+VALUES (
+    'N1',
+    '2',
+    '100',
+    '1'
+);
+
+INSERT INTO "room" (name, capacity, price, coworking_id)
+VALUES (
+    'E1',
+    '5',
+    '300',
+    '1'
+);
+
+INSERT INTO "room" (name, capacity, price, coworking_id)
+VALUES (
+    'W1',
+    '3',
+    '200',
+    '1'
+);
+
+INSERT INTO "room" (name, capacity, price, coworking_id)
+VALUES (
+    'S1',
+    '1',
+    '60',
+    '1'
+);
+
+-- CoWorking2
+INSERT INTO "room" (name, capacity, price, coworking_id)
+VALUES (
+    'IchiHeya',
+    '10',
+    '1000',
+    '2'
+);
+
+INSERT INTO "room" (name, capacity, price, coworking_id)
+VALUES (
+    'NiHeya',
+    '5',
+    '500',
+    '2'
+);
+
+-- CoWorking3
+INSERT INTO "room" (name, capacity, price, coworking_id)
+VALUES (
+    'FirstRoom',
+    '1',
+    '50',
+    '3'
+);
+
+INSERT INTO "room" (name, capacity, price, coworking_id)
+VALUES (
+    'SecondRoom',
+    '50',
+    '20000',
+    '3'
+);
