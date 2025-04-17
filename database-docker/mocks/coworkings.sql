87faaf43eb451bd2b3f3fd70aec8255d3a8701e0
TeaChanathip <teachanthip@gmail.com>
2025-04-17 23:04:44 +0700

    Add mocks coworking data

---
+++ database-docker/mocks/coworkings.sql
@@ -0,0 +1,63 @@
+INSERT INTO "coworking" (name, address, phone, open_time, close_time)
+VALUES (
+    'HiveWorks Hub',
+    '123 Sukhumvit Road, Khlong Toei, Bangkok 10110',
+    '+66 811111111',
+    '06:00:00',
+    '18:00:00'
+);
+
+INSERT INTO "coworking" (name, address, phone, open_time, close_time)
+VALUES (
+    'The Creative Loft',
+    '45 Nimmanhaemin Road, Mueang, Chiang Mai 50200',
+    '+66 822222222',
+    '09:00:00',
+    '22:00:00'
+);
+
+INSERT INTO "coworking" (name, address, phone, open_time, close_time)
+VALUES (
+    'Urban Desk Collective',
+    '789 Ratchadaphisek Road, Din Daeng, Bangkok 10400',
+    '+66 833333333',
+    '08:00:00',
+    '20:00:00'
+);
+
+INSERT INTO "coworking" (name, address, phone, open_time, close_time)
+VALUES (
+    'Innovate Space',
+    '56 Phahonyothin Road, Hat Yai, Songkhla 90110',
+    '+66 844444444',
+    '07:00:00',
+    '18:00:00'
+);
+
+INSERT INTO "coworking" (name, address, phone, open_time, close_time)
+VALUES (
+    'The Work Nook',
+    '32 Rama IV Road, Pathum Wan, Bangkok 10330',
+    '+66 855555555',
+    '00:00:00',
+    '23:59:59'
+);
+
+INSERT INTO "coworking" (name, address, phone, open_time, close_time)
+VALUES (
+    'Collab Corner',
+    '98 Sathorn Road, Yannawa, Bangkok 10120',
+    '+66 866666666',
+    '00:00:00',
+    '23:59:59'
+);
+
+INSERT INTO "coworking" (name, address, phone, open_time, close_time)
+VALUES (
+    'NextGen Workspace',
+    '12 Walking Street, Mueang, Pattaya, Chonburi 20150',
+    '+66 877777777',
+    '05:00:00',
+    '21:00:00'
+);
+
--- database-docker/mocks/users.sql
+++ database-docker/mocks/users.sql
@@ -2,38 +2,48 @@
 
 -- Insert Admins
 INSERT INTO "user" (name, phone, email, password, role)
-VALUES ('admin1', 
-        '+66 912345678', 
-        'admin1@gmail.com', 
-        '$2b$10$Z3YPCsGRsoZpR0JnsST48eKQTuBWI8j01ghTZ47JT7VqmRX5ypJg.', 
-        'admin');
+VALUES (
+    'admin1', 
+    '+66 912345678', 
+    'admin1@gmail.com', 
+    '$2b$10$Z3YPCsGRsoZpR0JnsST48eKQTuBWI8j01ghTZ47JT7VqmRX5ypJg.', 
+    'admin'
+);
 
 INSERT INTO "user" (name, phone, email, password, role)
-VALUES ('admin2', 
-        '+66 987654321', 
-        'admin2@gmail.com', 
-        '$2b$10$1a5XeF/avgxU7Jm/N3fNRuX7qtnMfkoTwrAIm//tGwT3idT5ve4a6', 
-        'admin');
+VALUES (
+    'admin2', 
+    '+66 987654321', 
+    'admin2@gmail.com', 
+    '$2b$10$1a5XeF/avgxU7Jm/N3fNRuX7qtnMfkoTwrAIm//tGwT3idT5ve4a6', 
+    'admin'
+);
 
 -- Insert Users
 INSERT INTO "user" (name, phone, email, password, role)
-VALUES ('user2', 
-        '+66 922222222', 
-        'user2@gmail.com', 
-        '$2b$10$qd2rZFR481eaiHmj.QY87.1/kBwjZLuAuUBwndBfZt14U7EwIxhXm', 
-        'user');
+VALUES (
+    'user2', 
+    '+66 922222222',
+    'user2@gmail.com', 
+    '$2b$10$qd2rZFR481eaiHmj.QY87.1/kBwjZLuAuUBwndBfZt14U7EwIxhXm', 
+    'user'
+);
 
 
 INSERT INTO "user" (name, phone, email, password, role)
-VALUES ('user3', 
-        '+66 933333333', 
-        'user3@gmail.com', 
-        '$2b$10$Pz4BzAv/oGpuebtbjjWzBOTsmw6wWVaOfNHDYH/fCn8abRZXe92jK', 
-        'user');
+VALUES (
+    'user3', 
+    '+66 933333333', 
+    'user3@gmail.com', 
+    '$2b$10$Pz4BzAv/oGpuebtbjjWzBOTsmw6wWVaOfNHDYH/fCn8abRZXe92jK', 
+    'user'
+);
 
 INSERT INTO "user" (name, phone, email, password, role)
-VALUES ('user4', 
-        '+66 944444444', 
-        'user4@gmail.com', 
-        '$2b$10$4drTk0goe.3nhUW4E1yjU.MyEWH6clnM/nz1Izn3iHXEeaxk5JtoW', 
-        'user');
+VALUES (
+    'user4', 
+    '+66 944444444', 
+    'user4@gmail.com', 
+    '$2b$10$4drTk0goe.3nhUW4E1yjU.MyEWH6clnM/nz1Izn3iHXEeaxk5JtoW', 
+    'user'
+);
